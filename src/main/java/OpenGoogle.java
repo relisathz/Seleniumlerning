317437d249f15f3bce1a4759880769eca68eda6a
relisathz <dula98sathsarani@gmail.com>
2025-12-07 19:51:20 +0530

    test selenium -Launch Chrome Browsers using Selenium WebDriver

---
+++ src/main/java/OpenGoogle.java
@@ -0,0 +1,17 @@
+public class OpenGoogle {
+
+    //open google
+    //go to google home page
+
+    public static void main(String[] args) {
+        //1st stage.........
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\DELL\\Documents\\driver\\chromedriver-win64\\chromedriver.exe");
+        //2nd stage.......
+        //WebDriverManager.chromedriver().setup();
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.google.com");
+//        driver.quit();
+//
+//    }
+    }
+}
